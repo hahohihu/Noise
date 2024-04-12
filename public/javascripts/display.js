72927c5158dfa18d7eff9280a06a5b8c406a6ac5
Peter Shen <pshen4@gmail.com>
2024-04-12 13:55:34 -0700

    basic chart

--- public/javascripts/display.js
+++ public/javascripts/display.js
@@ -9,7 +9,7 @@ class SpotifyAPI {
 		this.access_token = access_token;
 	}
 
-	async request(path, method='GET') {
+	async request(path, method = 'GET') {
 		let res = await fetch(path, {
 			method: method,
 			headers: {
@@ -20,7 +20,7 @@ class SpotifyAPI {
 	}
 
 	get(path) {
-		return this.request('https://api.spotify.com/v1' + path);	
+		return this.request('https://api.spotify.com/v1' + path);
 	}
 
 	async getSavedTracks() {
@@ -41,8 +41,8 @@ class SpotifyAPI {
 			if (tracks && Array.isArray(tracks)) {
 				return tracks;
 			}
-		} catch (e) {}
-	
+		} catch (e) { }
+
 		tracks = await spotify.getSavedTracks();
 		localStorage.setItem('tracks', JSON.stringify(tracks));
 		return tracks;
@@ -57,53 +57,49 @@ let spotify = new SpotifyAPI(token);
 
 (async () => {
 	let me = await spotify.get('/me');
+	console.log(me);
 	document.getElementById('filler').innerText = "Hello, " + me.display_name + "!";
 })();
 
 (async () => {
 	let tracks = await spotify.getSavedTracksCached();
 	console.log(tracks);
+
+	let data = tracks.map(t => {
+		return {
+			name: t.track.name,
+			artists: t.track.artists,
+			duration: t.track.duration_ms,
+			popularity: t.track.popularity
+		}
+	});
+	let durations = data.map(t => t.duration);
+	let popularities = data.map(t => t.popularity);
+
+	var width = 600, height = 400;
+	var colorScale = ['orange', 'lightblue', '#B19CD9'];
+
+	let durScale = d3.scaleLinear()
+		.domain([Math.min(...durations), Math.max(...durations)])
+		.range([0, width])
+
+	let popScale = d3.scaleLinear()
+		.domain([Math.min(...popularities), Math.max(...popularities)])
+		.range([0, height]);
+
+	d3.select('svg g')
+		.selectAll('circle')
+		.data(data)
+		.join('circle')
+		.attr('r', 3)
+		.attr('cx', d => durScale(d.duration))
+		.attr('cy', d => popScale(d.popularity))
+		.on('mouseover', function(d) {
+			d3.select(this).style("fill", "red");
+		}).on("mouseout", function(d) {
+			d3.select(this).style("fill", "black");
+		});
 })();
 
-var width = 600, height = 400;
 
-var colorScale = ['orange', 'lightblue', '#B19CD9'];
-var xCenter = [100, 300, 500];
-
-var numNodes = 100;
-var nodes = d3.range(numNodes).map(function (d, i) {
-	return {
-		radius: Math.random() * 25,
-		category: i % 3
-	}
-});
-
-var simulation = d3.forceSimulation(nodes)
-	.force('charge', d3.forceManyBody().strength(5))
-	.force('x', d3.forceX().x(function (d) {
-		return xCenter[d.category];
-	}))
-	.force('collision', d3.forceCollide().radius(function (d) {
-		return d.radius;
-	}))
-	.on('tick', ticked);
-
-function ticked() {
-	var u = d3.select('svg g')
-		.selectAll('circle')
-		.data(nodes)
-		.join('circle')
-		.attr('r', function (d) {
-			return d.radius;
-		})
-		.style('fill', function (d) {
-			return colorScale[d.category];
-		})
-		.attr('cx', function (d) {
-			return d.x;
-		})
-		.attr('cy', function (d) {
-			return d.y;
-		});
-}
 
